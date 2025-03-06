28b8a2e8c1583830218e8ca33c8a2b84f3e61e11
David Mercado <dmerc90@gmail.com>
2025-03-05 20:38:27 -0800

    Alternative solution

--- dow.py
+++ dow.py
@@ -3,11 +3,11 @@ han = open('mbox-short.txt')
 for line in han:
     line = line.rstrip()
     print('Line:',line)
+    if line == '' :
+        print('Skip Blank')
+        continue
     wds = line.split()
     print('Words:',wds)
-    # Guardian pattern
-    if len(wds) < 1 :
-        continue
     # Potentially faulty code (without guardian pattern)
     if wds[0] != 'From' :
         print('Ignore')
