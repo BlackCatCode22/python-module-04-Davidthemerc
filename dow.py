4058902e50243ec21cebb579a87eae79e8ddbd02
David Mercado <dmerc90@gmail.com>
2025-03-05 20:37:18 -0800

    Neat! It was because of a blank line. Guardian pattern code should prevent a recurrence of this issue.

--- dow.py
+++ dow.py
@@ -2,8 +2,13 @@ han = open('mbox-short.txt')
 
 for line in han:
     line = line.rstrip()
+    print('Line:',line)
     wds = line.split()
     print('Words:',wds)
+    # Guardian pattern
+    if len(wds) < 1 :
+        continue
+    # Potentially faulty code (without guardian pattern)
     if wds[0] != 'From' :
         print('Ignore')
         continue
