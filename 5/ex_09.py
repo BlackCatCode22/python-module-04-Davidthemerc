5c6da53dab69bb47b1fa214b2f1eb16bd945a475
David Mercado <dmerc90@gmail.com>
2025-03-22 19:23:13 -0700

    Printing the most common word and the number of times it occurs; don't mind the car error

--- 5/ex_09.py
+++ 5/ex_09.py
@@ -5,13 +5,21 @@ hand = open(fname)
 di = dict()
 for lin in hand:
     lin = lin.rstrip()
-    # print (lin)
     wds = lin.split()
-    # print(wds)
     for w in wds:
-
         # idiom: retrieve/create/update counter
         di[w] = di.get(w,0) + 1
-        print(w,'new',di[w])
+        # print(w,'new',di[w])
 
-print(di)
+# print(di)
+
+# now we want to find the most common word
+largest = -1
+theword = None
+for k,v in di.items() :
+    print (k,v)
+    if v > largest :
+        largest = v
+        theword = k #capture/remember the word that was largest
+
+print('Done',theword,largest)
