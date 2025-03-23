98b5c7485f9ea4b746337146aa6b1e9f9c78e25b
David Mercado <dmerc90@gmail.com>
2025-03-22 19:13:56 -0700

    Added oldcount, newcount code

--- 5/ex_09.py
+++ 5/ex_09.py
@@ -9,13 +9,14 @@ for lin in hand:
     wds = lin.split()
     # print(wds)
     for w in wds:
-        print('**',w,di.get(w,-99))
 
-        if w in di :
-            di[w] = di[w] + 1
-        else:
-            di[w] = 1
+        # if the key is not there, the count is zero
+        oldcount = di.get(w,0)
+        print(w,'old',oldcount)
+        newcount = oldcount + 1
+        di[w] = newcount
 
+        print(w,'new',newcount)
         # print(w,di[w])
 
 print(di)
