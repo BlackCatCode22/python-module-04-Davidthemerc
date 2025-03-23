00b9c00db684b375c7b007df1eb6b017e439c5ec
David Mercado <dmerc90@gmail.com>
2025-03-22 19:05:29 -0700

    Added existing print statement; moved print(w) to top of inner loop

--- 5/ex_09.py
+++ 5/ex_09.py
@@ -9,9 +9,11 @@ for lin in hand:
     wds = lin.split()
     # print(wds)
     for w in wds:
+        print(w)
         if w in di :
             di[w] = di[w] + 1
+            print ('**Existing**')
         else:
             di[w] = 1
             print ('**NEW**')
-        print(w,di[w])
+        print(di[w])
